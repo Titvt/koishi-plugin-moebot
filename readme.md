ae757270f53b6b8a8d5ec30c89357d359edb5c9c
Titvt <tqydyqt@126.com>
2024-12-10 10:31:49 +0800

    1.0.0

--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
 # koishi-plugin-moebot
 
-[![npm](https://img.shields.io/npm/v/koishi-plugin-moebot?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-moebot)
-
-
+Koishi Plugin MoeBot
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
 
-export const name = 'moebot'
+export const name = "Koishi Plugin MoeBot";
 
 export interface Config {}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.on("message", (session) => {
+    if (session.content === "天王盖地虎") {
+      session.send("宝塔镇河妖");
+    }
+  });
 }
